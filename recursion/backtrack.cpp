0b1c48be430aff0275db76d5d66c09711642fa61
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-20 22:53:00 +0530

    some changes

---
+++ recursion/backtrack.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+void printwhole(int n){
+    if(n==-1){
+        
+        return;
+    }
+
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter a number:"
+    printwhole(n);
+    
+    return 0;
+}
