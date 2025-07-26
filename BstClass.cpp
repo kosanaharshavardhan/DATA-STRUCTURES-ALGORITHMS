0244623bbbd85a263d4264dff71a67fb2cbbcef5
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-07-26 13:54:59 +0530

    Bst half done

--- BstClass.cpp
+++ BstClass.cpp
@@ -1,11 +1,69 @@
-class TreeNode{
-    int data;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int val){
-        this->data = val;
-        this->left=nullptr;
-        this->right=nullptr;
+#include <iostream>
+using namespace std;
+class BstClass{
+public:
+    class TreeNode{
+        public:
+            int data;
+            TreeNode* left;
+            TreeNode* right;
+            TreeNode(int val){
+                this->data = val;
+                this->left=nullptr;
+                this->right=nullptr;
+            }
+    };
+     TreeNode* root=nullptr;
+    TreeNode* insert(TreeNode* root, int val){
+        if(root==nullptr){
+            TreeNode* newnode= new TreeNode(val);
+            return newnode;
+        }
+        if(root->data>val){
+            root->left=insert(root->left,val);
+        }
+        else{
+            root->right=insert(root->right,val);
+        }
+        return root;
+    }
+    TreeNode* search(TreeNode* root, int val){
+        if(root==nullptr){
+            return nullptr;
+        }
+        if(root->data==val){
+            return root;
+        }
+        else if(root->data>val){
+            return search(root->left,val);
+        }
+        else{
+            return search(root->right,val);
+        }
+    }
+    void traversal(TreeNode* root){
+        if(root==nullptr){
+            return;
+        }
+        traversal(root->left);
+        cout<<root->data<<" ";
+        traversal(root->right);
+    }
+    TreeNode* remove(TreeNode* root, int val){
+        return nullptr;
     }
 };
+ int main(){
+        BstClass bt;
+        bt.root=bt.insert(bt.root,10);
+        bt.root=bt.insert(bt.root,20);
+        bt.root=bt.insert(bt.root,5);
+        bt.traversal(bt.root);
+
+    }
+
+
+
+
+
 
