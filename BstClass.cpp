4c31cf723b30ced820b7b0388d1841f64022a859
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-07-26 11:54:11 +0530

    normal commit

---
+++ BstClass.cpp
@@ -0,0 +1,11 @@
+class TreeNode{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val){
+        this->data = val;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+};
+
--- inout.cpp
+++ inout.cpp
@@ -4,6 +4,7 @@ int main(){
     string s;
     // cin>>s;
     // for multiple spaabes
+    cout<<"Enter a string:";
     getline(cin,s);
     cout<<s;
     return 0;
