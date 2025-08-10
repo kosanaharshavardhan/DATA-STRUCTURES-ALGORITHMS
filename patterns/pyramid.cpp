f023fef1425360ca644a6e24a2978067d8a8dbef
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-08-10 13:21:26 +0530

    variatipns

--- patterns/pyramid.cpp
+++ patterns/pyramid.cpp
@@ -4,7 +4,16 @@ int main(){
     int n;
     cout<<"enter num:";
     cin>>n;
-    for(int i=n-1;i>-1;i--){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<(2*n)-1;j++){
+            if(j<n-i-1) cout<<" ";
+            else if(j>n+i-1) cout<<" ";
+            else cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+    for(int i=n-2;i>-1;i--){
         for(int j=0;j<(2*n)-1;j++){
             if(j<n-i-1) cout<<" ";
             else if(j>n+i-1) cout<<" ";
