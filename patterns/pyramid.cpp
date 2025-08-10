5fdbc0bb722479df1249bc391b371bcc29927747
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-08-10 13:19:56 +0530

    upside pyramid down side pyramid done

---
+++ patterns/pyramid.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter num:";
+    cin>>n;
+    for(int i=n-1;i>-1;i--){
+        for(int j=0;j<(2*n)-1;j++){
+            if(j<n-i-1) cout<<" ";
+            else if(j>n+i-1) cout<<" ";
+            else cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+}
